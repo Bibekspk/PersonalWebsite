40efd874aaa983d2a4103fa061361bee29f2f570
Bibekspk <bibeksp7@gmail.com>
2023-02-11 20:39:52 +0545

    changes

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,7 +10,48 @@ import { Avatar } from "antd";
 const Main = () => {
   const [text, setText] = useState([]);
   const [count, setCount] = useState(0);
-  let name = ["I", " ", "a", "m", " ", "a", " ", "F", "u", "l", "l", " ", "S", "t", "a", "c", "k", " ", "W", "e", "b", " ", "D", "e", "v", "e", "l", "o", "p", "e", "r"];
+  let [imgWidth, setImgWidth] = useState();
+
+  const updateDimensions = () => {
+    if (innerWidth < 767) {
+      setImgWidth("300px");
+    } else {
+      setImgWidth("425px");
+    }
+  };
+  useEffect(() => {
+    updateDimensions();
+  }, []);
+
+
+  let name = [
+    "A",
+    " ",
+    "F",
+    "u",
+    "l",
+    "l",
+    " ",
+    "S",
+    "t",
+    "a",
+    "c",
+    "k",
+    " ",
+    "W",
+    "e",
+    "b",
+    " ",
+    "D",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+  ];
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -19,26 +60,27 @@ const Main = () => {
         setText([...text, name[count]]);
         setCount(count + 1);
       }
-      // if (count === lengthData) {
-      //   setText([]);
-      //   setCount(0);
-      // }
     }, 100);
 
     return () => clearInterval(interval);
   }, [count]);
 
+  typeof window !== "undefined" &&
+  window.addEventListener("resize", updateDimensions);
+
   return (
     <>
       <div
         id="home"
-        className="text-center grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2  mt-[100px] px-[20px]"
+        className="text-center sm:h-screen grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 sm:mt-[100px] md:mt-[40px]  mt-[100px] px-[20px]"
       >
-        <div className="flex items-center justify-center">
-          <Avatar
-            shape="circle"
-            size={320}
-            src={<Image src={NavLogo} alt="avatar" />}
+        <div className="flex items-center justify-center mb-1">
+          <Image
+            src={NavLogo}
+            width={imgWidth}
+            height={imgWidth}
+            alt="avatar"
+            className="rounded-full"
           />
         </div>
         <div className="mx-auto p-2 flex justify-center items-center">
@@ -46,20 +88,19 @@ const Main = () => {
             <h1 className="text-gray-700">
               I&#39;m <span className="text-[#1285CB]"> Bibek</span>
             </h1>
-            {
-             Array.isArray(text) && text?.length > 0 ?
-              <h1 className="py-2 text-black-500 text-center text-3xl"> {text}</h1>
-              :
+            {Array.isArray(text) && text?.length > 0 ? (
+              <h1 className="py-2 text-black-500 text-center text-3xl">
+                {" "}
+                {text}
+              </h1>
+            ) : (
               <h1 className="py-2 text-blue-500 text-center text-2xl">""</h1>
-              
-            }
+            )}
 
             <p className="py-2 text-gray-600 sm:max-w-[70%] m-auto">
               I am a skilled and motivated <strong>Full Stack Developer</strong>{" "}
               with extensive experience in building web applications using the
-              stacks. My expertise lies in leveraging MongoDB, PostGreSQL,
-              Express, React, Next, Loopback and Node.js to create dynamic and
-              scalable web solutions that meet your demands.
+              stacks.
               <br></br>
               <strong> Gmail </strong>: bibeksp7@gmail.com
             </p>
@@ -82,11 +123,11 @@ const Main = () => {
                   <FaLinkedinIn />
                 </div>
               </a>
-              {/* <Link href="/#contact">
+              <Link href="/#contact">
               <div className="rounded shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                 <AiOutlineMail />
               </div>
-            </Link> */}
+            </Link>
               {/* <Link href="/resume">
               <div className="rounded shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                 <BsFillPersonLinesFill />
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -24,17 +24,17 @@ const Skills = () => {
       <div className="mx-auto flex flex-col justify-center">
         <h2 className="py-4">Skills</h2>
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105  ease-in duration-300 hover:cursor-pointer">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="flex flex-col items-center justify-center">
                 <h3>JavaScript</h3>
               </div>
-              <div className="m-auto">
-                <Image src={Javascript} width="64px" height="64px" alt="/" />
+              <div className="m-auto hover:rotate-[360deg]">
+                <Image  src={Javascript} width="64px" height="64px" alt="/" />
               </div>
             </div>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 hover:cursor-pointer">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="flex flex-col items-center justify-center">
                 <h3>Node</h3>
@@ -45,7 +45,7 @@ const Skills = () => {
             </div>
           </div>
 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 hover:cursor-pointer">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image src={ReactImg} width="64px" height="64px" alt="/" />
@@ -55,7 +55,7 @@ const Skills = () => {
               </div>
             </div>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 hover:cursor-pointer">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="flex flex-col items-center justify-center">
                 <h3>Next</h3>
@@ -65,7 +65,7 @@ const Skills = () => {
               </div>
             </div>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 hover:cursor-pointer">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="flex flex-col items-center justify-center">
                 <h3>Loopback</h3>
@@ -75,7 +75,7 @@ const Skills = () => {
               </div>
             </div>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 hover:cursor-pointer">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="flex flex-col items-center justify-center">
                 <h3>TypeScript</h3>
@@ -85,7 +85,7 @@ const Skills = () => {
               </div>
             </div>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 hover:cursor-pointer">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="flex flex-col items-center justify-center">
                 <h3>MongoDB</h3>
@@ -95,7 +95,7 @@ const Skills = () => {
               </div>
             </div>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 hover:cursor-pointer">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="flex flex-col items-center justify-center">
                 <h3>HTML</h3>
@@ -105,7 +105,7 @@ const Skills = () => {
               </div>
             </div>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 hover:cursor-pointer">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="flex flex-col items-center justify-center">
                 <h3>CSS</h3>
@@ -116,7 +116,7 @@ const Skills = () => {
             </div>
           </div>
 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 hover:cursor-pointer">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="flex flex-col items-center justify-center">
                 <h3>Tailwind</h3>
@@ -126,17 +126,17 @@ const Skills = () => {
               </div>
             </div>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 hover:cursor-pointer">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="flex flex-col items-center justify-center">
-                <h3>PostGRES</h3>
+                <h3>PostGres</h3>
               </div>
               <div className="m-auto">
                 <Image src={Postgres} width="64px" height="64px" alt="/" />
               </div>
             </div>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 hover:cursor-pointer">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="flex flex-col items-center justify-center">
                 <h3>MySQL</h3>
